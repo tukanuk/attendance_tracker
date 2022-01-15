bb85104c2b702edcfe0595fb028a9ddb257905d2
benjamin <benjamin@idavidson.ca>
2022-01-14 22:44:24 -0500

    working notebook for meetingattendance processing

---
+++ settings.py
@@ -0,0 +1,16 @@
+""" Stores settings using dotenv. This file can be included in version
+control and contains references to environment variables but checks
+if the variable is present in a .env file (which is not included in
+version control) and uses those values first
+
+Every variable should be included here even it is not present in Environment
+variables.
+"""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+attendance_report_path = os.getenv("ATTENDANCE_REPORT_PATH")
