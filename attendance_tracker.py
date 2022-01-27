6fcb5b528ae76cf14ecd1a8d42e0e2c04d4c54b3
benjamin <benjamin@idavidson.ca>
2022-01-26 22:42:39 -0500

    added filtering of raw meeting file

--- attendance_tracker.py
+++ attendance_tracker.py
@@ -23,20 +23,85 @@ log.info("Starting logging")
 def main():
     """Main function"""
 
-    filePath = command_line_parser()
+    filePath, processing_meeting_attendance_reports = command_line_parser()
     filePath, fileList = file_processor(filePath)
 
+    # log the task parameters
     log.info("path: %s", filePath)
     log.info("files: %s", fileList)
+    log.info(
+        "Processing MeetingAttendanceReports: %s",
+        processing_meeting_attendance_reports,
+    )
 
-    df = build_list(filePath, fileList)
+    if processing_meeting_attendance_reports:
+        df = build_list_from_raw(filePath, fileList)
+    else:
+        df = build_simple_list(filePath, fileList)
 
     csvExport(filePath, df)
 
     # print(df)
 
 
-def build_list(filePath, fileList):
+def build_list_from_raw(filePath, fileList):
+    """Build an email list from a raw Teams attendance file"""
+
+    df = pd.DataFrame()
+    df["Email"] = ""
+
+    for item in fileList:
+        log.info("Processing %s", item)
+
+        # may need to add a try block here to try utf-16 as well. I seem to be seeing both encodings
+        with open(f"{filePath}/{item}", "r", encoding="utf-8") as fp:
+            curr_file = fp.read()
+
+        # Get the session title
+        regex = r"Title\t(.*)\n"
+        matches = re.findall(regex, curr_file)
+        session_title = matches[0]
+        log.info("Title: %s", session_title)
+
+        # Get the session date
+        regex = r"Meeting Start Time\t(\d+)/(\d+)/(\d+),"
+        matches = re.findall(regex, curr_file)
+        print(matches[0])
+        date = f"{matches[0][2]}-{int(matches[0][0]):02}{int(matches[0][1]):02}"
+        log.info("Session dae: %s", date)
+
+        # Get the email addresess
+        regex = r"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}"
+        matches = re.findall(regex, curr_file, re.IGNORECASE)
+
+        # make lower case
+        emails = [x.lower() for x in matches]
+        emails.sort()
+        log.debug(emails)
+
+        # remove duplicate emails
+        log.info("Original list is %d long.", len(emails))
+        cleaned_emails = []
+        for email in emails:
+            # looking for user name matches (ignore domain)
+            name = email.split("@")[0]
+
+            if not re.search(rf"{name}", str(cleaned_emails), re.IGNORECASE):
+                cleaned_emails.append(email)
+
+        log.info(
+            "The cleaned and sorted email list is %d emails long and contains: %s",
+            len(cleaned_emails),
+            cleaned_emails,
+        )
+
+        # now add this to the df and do the same things as the other function
+        # return the info for the filename
+
+    return df
+
+
+def build_simple_list(filePath, fileList):
     """Opens a file or directory of .csv files"""
     df = pd.DataFrame()
     df["Email"] = ""
@@ -46,7 +111,7 @@ def build_list(filePath, fileList):
         # get the col name from the file
         col_name = item.split("_")[0]
 
-        try: 
+        try:
             data = pd.read_csv(f"{filePath}/{item}", names=["Email"])
 
             # get attendance for that day
@@ -58,9 +123,10 @@ def build_list(filePath, fileList):
             log.info("End of this file pass. df to carry over\n%s", df)
 
         except:
-            log.error("Couldn't make any sense out of %s. I'm going to skip this.", item)
+            log.error(
+                "Couldn't make any sense out of %s. I'm going to skip this.", item
+            )
             pass
-       
 
     # Groupby email. Max will prefer True
     df = df.groupby(["Email"]).max()
@@ -103,18 +169,27 @@ def command_line_parser():
         help="Include a .csv of a folder containing .csv",
     )
 
+    parser.add_argument(
+        "-r",
+        "--raw",
+        action="store_true",
+        default=False,
+        help="Process unaltered meetingAttendanceReport.csv files",
+    )
+
     # parser.add_argument("-hu", "--hostunits", required=True,
     #                     help="The account host unit limit")
     args = parser.parse_args()
     log.debug(f"Here are the args: {args}")
 
     filePath = args.raw_data_file_or_folder
+    processing_meeting_Attendance_Reports = args.raw
 
     # assert filePath == "data/2021-1102_Customer_Cohort_1.csv"
 
     log.info(f"The file path is: {filePath}")
 
-    return filePath
+    return filePath, processing_meeting_Attendance_Reports
 
 
 def file_processor(filePath):
